50dc97ac5fe5d507243fa7eff2460e5529ffd953
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2026-03-12 11:18:26 -0400

    Update ex03.py

--- aula1203/ex03.py
+++ aula1203/ex03.py
@@ -3,8 +3,9 @@ matriz =[]
 for i in range(3):
     linha = []
     for j in range(4):
-        linha.append(i * j)
+        linha.append((i+1) * (j+1))
     matriz.append(linha)
 
 for linha in matriz:
+
     print(linha)
