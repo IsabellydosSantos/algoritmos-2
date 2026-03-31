551b5bba74c4996f795df0a8c54a9393acdb5bf2
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2026-03-31 11:10:03 -0400

    Add files via upload

---
+++ aula3103/exemplo03.py
@@ -0,0 +1,22 @@
+def letra(texto):
+    if not texto:
+        return "String vazia"
+
+    contagem = {}
+
+    for letra in texto:
+        if letra.isalpha():
+            contagem[letra] = contagem.get(letra, 0) + 1
+
+    letra_freq = max(contagem.values())
+
+    for letra in texto:
+        if contagem[letra] == letra_freq:
+            return letra
+
+
+texto = input("Insira uma string: ").strip().lower()
+
+resultado = letra(texto)
+
+print(f"A letra mais comum é: {resultado}")
