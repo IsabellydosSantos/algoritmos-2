9449177bf6e78661702b2398aaaa91eaebc58be3
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2026-04-09 14:01:24 -0400

    Update ex04.py

--- listaexercicios1/ex04.py
+++ listaexercicios1/ex04.py
@@ -1,3 +1,13 @@
 valorc = float(input("Qual o valor da casa a ser comprada? "))
 sal = float(input("Qual o valor do seu salário? "))
 anos = float(input("Qual a quantidade de anos a pagar? "))
+
+mes = anos * 12
+prest = valorc/mes
+psal = sal * 0.3
+
+if prest > psal:
+  print("Seu empréstimo foi negado.")
+else:
+  print("Seu empréstimo foi aprovado.")
+  
