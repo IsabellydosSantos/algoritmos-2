da437abba03c46c9f5c53ecf2d2698f1cdfa6047
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2026-04-09 14:46:09 -0400

    Update ex06.py

---
+++ listaexercicios1/ex06.py
@@ -0,0 +1,26 @@
+print("Insira apenas números inteiros. Digite 0 para encerrar")
+
+soma = 0
+contador = 0
+
+while True:
+  try:
+    n = float(input("Digite um número: "))
+
+    if n == 0:
+      break
+
+    soma += n
+    contador += 1
+
+  except ValueError:
+    print("Digite apenas números inteiros")
+    
+if contador > 0:
+  media = soma/contador
+  print(f"Quantidade total de números: {contador}")
+  print(f"Soma total: {soma:.2f}")
+  print(f"Média Aritmética: {media:.2f}")
+else:
+  print("Nenhum número foi digitado antes do 0")
+  
