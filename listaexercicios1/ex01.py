119b5e5ca489c64ffba16b8069723dea993809ab
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2026-04-09 13:53:35 -0400

    Update ex01.py

--- listaexercicios1/ex01.py
+++ listaexercicios1/ex01.py
@@ -1,4 +1,4 @@
-def calc():
+def calc(dias,km):
     aluguel = dias * 60
     kmt = km * 0.15
     total = aluguel + kmt
@@ -9,6 +9,6 @@ def calc():
 dias = float(input("Por quantos dias o carro foi alugado? "))
 km = float(input("Quantos KMs foram percorridos? "))
 
-resultado = calc()
+resultado = calc(dias,km)
 
 print(f"O preço total a ser gasto é de R${resultado}")
