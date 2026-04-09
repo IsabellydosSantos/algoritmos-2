3dd855aec8c2416306c636145b2644e778468e29
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2026-04-09 10:15:19 -0400

    Update ex21.py

--- revisaop1/ex21.py
+++ revisaop1/ex21.py
@@ -1,10 +1,32 @@
-def tri(n):
-    
+def num_tri(n):
+    if n < 0:
+        return False, None
+    elif n == 0:
+        return True, (0, 1, 2)
+
+    i = 0
+    while i * (i + 1) * (i + 2) <= n:
+        if i * (i + 1) * (i + 2) == n:
+            return True, (i, i + 1, i + 2)
+        i += 1
+    return False, None
 
 
 
 try:
-    n = int(input("Insira um número intero não negativo"))
+    n = int(input("Insira um número inteiro positivo: "))
+
+    if n < 0:
+        print("Digite apenas números positivos")
+
+    resultado, numeros = num_tri(n)
+
+    print(f"Retorno da função: {resultado}")
+
+    if resultado:
+            print(f"O número {n} é triangular e é formado por {numeros[0]} * {numeros[1]} * {numeros[2]}")
+    else:
+            print(f"O número {n} não é triangular")
 
 except ValueError:
-    print("Insira apenas valores inteiros")
+    print("Digite apenas números inteiros")
