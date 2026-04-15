12ead9d5b04d83f4b17a4eefbf88ae774acbeef7
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2026-04-15 17:03:26 -0400

    Update ex08.py

--- listaexercicios1/ex08.py
+++ listaexercicios1/ex08.py
@@ -1,9 +1,9 @@
 # Programa para somar duas listas elemento por elemento
 
-entrada1 = input("\nDigite os números da primeira lista (separados por vírgula): ")
+entrada1 = input("Insira os números da primeira lista (separados por vírgula): ")
 lista1 = [float(x.strip()) for x in entrada1.split(',')]
 
-entrada2 = input("Digite os números da segunda lista (separados por vírgula): ")
+entrada2 = input("Insira os números da segunda lista (separados por vírgula): ")
 lista2 = [float(x.strip()) for x in entrada2.split(',')]
 
 if len(lista1) != len(lista2):
@@ -16,7 +16,6 @@ if len(lista1) != len(lista2):
         lista1.append(0)
     while len(lista2) < tamanho_max:
         lista2.append(0)
-    
     
 lista_soma = [a + b for a, b in zip(lista1, lista2)]
 
