5b853f098fbfd0d3f07ddd118177b7379479044c
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2026-05-05 09:37:50 -0400

    Add files via upload

---
+++ aula2804/arquivo.py
@@ -0,0 +1,18 @@
+try:
+    arquivo = open("notas.txt", "r")
+
+    for linha in arquivo:
+        linha = linha.strip()
+
+        parte = linha.split()
+        nome = parte[0]
+
+        notas = []
+        for i in range(1, len(parte)):
+            notas.append(float(parte[i]))
+
+        if len(notas) > 6:
+            media = sum(notas)/len(notas)
+            print(f"Estudante: {nome}\nMédia: {media:.2f}")
+except:
+    print("Não foi possível abrir o arquivo.")
