b982560caed22749b68de5c8c5a8c34c3dca8e97
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2026-05-07 10:32:19 -0400

    Substitui strings em arquivos

--- aula0505/exercicio32/ex32.py
+++ aula0505/exercicio32/ex32.py
@@ -1,10 +1,10 @@
 def substituir(arquivo1, arquivo2, string1, string2):
     try:
-        with open(arquivo1, "r") as original:
+        with open(arquivo1, 'r', encoding='utf-8') as original:
             conteudo = original.read()
         conteudo_mod = conteudo.replace(string1, string2)
 
-        with open(arquivo2, 'w') as modif:
+        with open(arquivo2, 'w', encoding='utf-8') as modif:
             modif.write(conteudo_mod)
         return True
     except FileNotFoundError:
@@ -17,8 +17,8 @@ def substituir(arquivo1, arquivo2, string1, string2):
 
 arquivo1 = input("Insira o nome do primeiro arquivo: ")
 arquivo2 = input("Insira o nome do segundo arquivo: ")
-string1 = input("Insira a primeira string: ")
-string2 = input("Insira a segunda string: ")
+string1 = input("Insira a primeira string: ").strip()
+string2 = input("Insira a segunda string: ").strip()
 
 resultado = substituir(arquivo1, arquivo2, string1, string2)
 
@@ -26,3 +26,4 @@ if resultado:
     print(f"Arquivo {arquivo2} criado com sucesso\nTodas as ocorrências de {string1} foram substituídas por {string2}")
 else:
     print(f"Erro. Verifique os dados e tente novamente")
+    
