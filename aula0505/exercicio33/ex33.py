c6038d28a424d0e4e5d582b5f9a0be51b65b4c33
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2026-05-13 19:16:50 -0400

    Update ex33.py

--- aula0505/exercicio33/ex33.py
+++ aula0505/exercicio33/ex33.py
@@ -2,32 +2,37 @@ def arquivos(arq_par, arq_imp, arq_saida):
     try:
         numeros = []
 
-        with open("pares.txt", "r") as pares:
+        with open(arq_par, "r") as pares:
             for linha in pares:
                 if linha.strip():
                     numeros.append(int(linha.strip()))
 
-        with open("impares.txt", "r") as impares:
+        with open(arq_imp, "r") as impares:
             for linha in impares:
                 if linha.strip():
                     numeros.append(int(linha.strip()))
 
         numeros.sort()
-        with open("paresimpares.txt", "w") as ambos:
+        
+        with open(arq_saida, "w") as ambos:
             for num in numeros:
                 ambos.write(f"{num}\n")
-                
+        
+        print(f"{len(numeros)} números salvos")
+        return True
+        
     except FileNotFoundError:
-        print(f"Erro: Algum dos arquivos não foi encontrado")
+        print("Erro: Algum dos arquivos não foi encontrado")
+        return False
     except ValueError:
-        print("Erro: O arquivo contém algo que não é um número")
+        print("Erro: O arquivo contém algo que não é número")
+        return False
 
 
-arq_par = "pares.txt"
-arq_impar = "impares.txt"
-arq_saida = "paresimpares.txt"
+resultado = arquivos("pares.txt", "impares.txt", "paresimpares.txt")
 
-resultado = arquivos(arq_par, arq_impar, arq_saida)
-
-if not resultado:
-    print("Erro. Verifique os arquivos e tente novamente")
+if resultado:
+    print("Operação concluída")
+else:
+    print("Falha na operação")
+    
