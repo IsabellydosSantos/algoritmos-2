6c16aa671be89b3576ed122a58d5def5fa4e0bf8
IsabellydosSantos <148358656+IsabellydosSantos@users.noreply.github.com>
2026-05-13 14:33:10 -0400

    Ordena listas por bubble sort

--- aula1205/ex36.py
+++ aula1205/ex36.py
@@ -2,10 +2,36 @@ def bubble_sort(lista):
     n = len(lista)
 
     for i in range(n-1):
-        trocou = False
+        print(f"\n {i+1}° Iteração")
+        troca = False
 
         for j in range(0, n-i-1):
             print(f"Comparando {lista[j]} e {lista[j+1]}")
             if lista[j] > lista[j+1]:
-                print(f"{lista[j]} > {lista[j + 1]}, troca")
-                
+                print(f"{lista[j]} > {lista[j + 1]}, trocando")
+                lista[j], lista[j+1] = lista[j+1], lista[j]
+                troca = True
+                print(f"→ Lista: {lista}")
+            else:
+                print(f"→ {lista[j]} < {lista[j+1]}, mantém → Lista: {lista}")
+        print("Fim da {i+1}° Iteração: {lista}")
+
+        if not troca:
+            print("\n Nenhuma troca nesta iteração. A lista já está ordenada")
+            break
+    return lista
+
+
+entrada = input("Insira os elementos da lista (separados por vírgula: ")
+
+try:
+    lista = [int(x) for x in entrada.split(',')]
+
+    if len(lista) == 0:
+        print("\n Nenhum número foi inserido.")
+    else:
+        lista_ordenada = bubble_sort(L.copy())
+        print(f" Lista ordenada: {lista_ordenada}")
+except ValueError:
+    print("\n Insira apenas números inteiros separados por vírgulas")
+    
